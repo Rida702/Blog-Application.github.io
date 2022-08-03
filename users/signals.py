855df1173b82d3eaefb0a0a8366fc2924a1445a4
Rida Shabbir <107110619+Rida702@users.noreply.github.com>
2022-08-03 10:40:31 -0700

    Update signals.py

--- users/signals.py
+++ users/signals.py
@@ -2,6 +2,7 @@ from django.db.models.signals import post_save
 from django.contrib.auth.models import User
 from django.dispatch import receiver
 from .models import Profile
+from django.core.exceptions import ObjectDoesNotExist
 
 @receiver(post_save, sender=User)
 def create_profile(sender, instance, created, **kwargs):
