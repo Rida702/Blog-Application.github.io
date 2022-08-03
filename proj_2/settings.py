9058d435a31791f07b66e6c53f58f80d649607a8
Rida Shabbir <107110619+Rida702@users.noreply.github.com>
2022-08-03 01:57:38 -0700

    Update settings.py

--- proj_2/settings.py
+++ proj_2/settings.py
@@ -81,7 +81,7 @@ WSGI_APPLICATION = 'proj_2.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR / 'db.sqlite3'),
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
